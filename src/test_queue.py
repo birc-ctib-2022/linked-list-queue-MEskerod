49d838b07f329dcf1ee683440b726a22b8c36d4b
MEskerod <112616513+MEskerod@users.noreply.github.com>
2022-11-29 19:48:42 +0100

    test_queue will not import from queue correctly

--- src/test_queue.py
+++ src/test_queue.py
@@ -1,6 +1,18 @@
 """Testing queues."""
 
+from queue import (Queue)
 
-def test_me() -> None:
-    """Remember to write tests."""
-    assert 2 + 2 == 4
+def test_make_queue(): 
+    queue = Queue()
+    assert str(queue) == "[]"
+
+def test_enqueue(): 
+    assert 2 == 1
+
+def test_is_empty(): 
+    queue = Queue()
+    assert queue.is_empty == True
+    queue.enqueue(1)
+    assert queue.is_empty == False
+
+
